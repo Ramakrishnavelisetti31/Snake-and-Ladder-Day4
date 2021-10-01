6ef06c74337525b3129b5e65feb40560a0adf45d
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-01 13:00:47 +0530

    Added: player Roll the Dice

--- src/main/java/com/bdlz/snakenladder/SnakeAndLadder.java
+++ src/main/java/com/bdlz/snakenladder/SnakeAndLadder.java
@@ -3,6 +3,8 @@ package com.bdlz.snakenladder;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int position = 0;
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
         System.out.println("The Starting Position of Player is : " + position);
+        System.out.println("Player Roll the Dice and get DiceNumber : " + diceNumber);
     }
 }
