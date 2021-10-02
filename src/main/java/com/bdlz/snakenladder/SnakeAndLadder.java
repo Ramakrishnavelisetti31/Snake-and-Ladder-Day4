03311ba7c683cf22a48d63c4ccebb1c83cd21200
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-02 11:56:05 +0530

    Added: Player Gets the exact Winnig Position

--- src/main/java/com/bdlz/snakenladder/SnakeAndLadder.java
+++ src/main/java/com/bdlz/snakenladder/SnakeAndLadder.java
@@ -3,19 +3,23 @@ package com.bdlz.snakenladder;
 public class SnakeAndLadder {
     public static final int IS_LADDER = 1;
     public static final int IS_SNAKE = 2;
-    public static final int NO_PLAY = 3;
     public static final int WINNING_POSITION = 100;
+
     public static void main(String[] args) {
         int position = 0;
         while (position < WINNING_POSITION) {
             int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
             System.out.println("DiceNumber is : " + diceNumber);
 
-            int option = (int) (Math.floor(Math.random() * 10) % 3 + 1);
+            int option = (int) (Math.floor(Math.random() * 10) % 2 + 1);
             switch (option) {
                 case IS_LADDER:
                     System.out.println("Ladder for the Player");
                     position = position + diceNumber;
+                    if (position > WINNING_POSITION) {
+                        System.out.println("No play stay in the Current Position");
+                        position = position - diceNumber;
+                    }
                     break;
                 case IS_SNAKE:
                     System.out.println("Snake for the player");
@@ -24,7 +28,7 @@ public class SnakeAndLadder {
                         position = 0;
                     }
                     break;
-                case NO_PLAY:
+                default:
                     System.out.println("No Play for the Player");
                     position = position;
                     break;
